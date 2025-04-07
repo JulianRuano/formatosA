407d8313e3d961e53f2e8a32f9c5f7c8ba8a6bdd
nimbachi <nimbachi@unicauca.edu.co>
2025-04-07 10:10:45 -0500

    Agregando metodo de crear Observaciones

--- src/main/java/com/jpa/JpaApplication.java
+++ src/main/java/com/jpa/JpaApplication.java
@@ -2,6 +2,7 @@ package com.jpa;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,11 +10,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jpa.models.Docente;
 import com.jpa.models.Estado;
+import com.jpa.models.Evaluacion;
 import com.jpa.models.FormatoA;
 import com.jpa.models.FormatoPPA;
-import com.jpa.models.FormatoTIA;
+import com.jpa.models.Observacion;
 import com.jpa.repository.IDocenteRepository;
+import com.jpa.repository.IEvaluacionRepository;
 import com.jpa.repository.IFormatoRepository;
+import com.jpa.repository.IObservacionRepository;
 
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +30,10 @@ public class JpaApplication implements CommandLineRunner {
 
 	private final IDocenteRepository docenteRepository;
 
+	private final IEvaluacionRepository evaluacionRepository;
+	private final IObservacionRepository observacionRepository;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
 	}
@@ -33,6 +41,8 @@ public class JpaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		//almacenarFormulario();
+		//List<Integer> docentes = List.of(1, 2);
+		//crearObservacion(docentes, "Observaciones de ROA", 1);
 	}
 
 	@Transactional
@@ -53,5 +63,49 @@ public class JpaApplication implements CommandLineRunner {
 		formatoRepository.save(formatoPPA);
 		
 	}
+
+	@Transactional
+	public void crearObservacion(List<Integer> docentes, String observacion, Integer idFormato) {
+		Optional<FormatoA> formataOptional = formatoRepository.findById(idFormato);
+		if (!formataOptional.isPresent()) {
+            throw new RuntimeException("FormatoA con id " + idFormato + " no encontrado.");
+        }
+
+		FormatoA formato = formataOptional.get();
+
+		Evaluacion evaluacion = null; //Intancia para guardar la evaluacion correspondite 
+		List<Evaluacion> lstEvaluaciones = formato.getEvaluacion();
+
+		if(lstEvaluaciones.isEmpty()) {
+
+			Evaluacion nuevaEvaluacion = new Evaluacion();
+			nuevaEvaluacion.setConcepto("Por establecer");
+			nuevaEvaluacion.setFechaRegistroConcepto(new Date());
+			nuevaEvaluacion.setObjFormato(formato);
+			evaluacion = evaluacionRepository.save(nuevaEvaluacion);
+
+		} else{
+
+			evaluacion = lstEvaluaciones.stream().filter(e -> "Por corregir".equals(e.getConcepto()))
+					.findFirst().orElse(null);
+
+		}
+
+		List<Docente> lstDocentes = docentes.stream().map(docenteId -> {
+			Docente d = docenteRepository.getReferenceById(docenteId);
+			return d;
+		}).toList();
+
+
+		Observacion nuevaObservacion = new Observacion();
+		nuevaObservacion.setObservacion(observacion);
+		nuevaObservacion.setFechaRegistro(new Date());
+		nuevaObservacion.setObjEvaluacion(evaluacion);
+		nuevaObservacion.setObjDocente(lstDocentes);
+
+		observacionRepository.save(nuevaObservacion);
+	}
+
+
 
 }
